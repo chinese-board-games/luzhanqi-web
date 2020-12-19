70726a4e395a3dc40bf5fac6f74f2aaa482ae68e
jon-jew <jonathan.jew@gmail.com>
2020-12-18 22:32:34 -0500

    added app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [listOfImages, setImages] = useState([]);
+
+  function importAll(r) {
+    return r.keys().map(r);
+  }
+
+  useEffect(() => {
+    setImages(
+      importAll(require.context("./MajongPieces/", false, /\.(png|jpe?g|svg)$/))
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {listOfImages.map((image, index) => {
+        console.log(image);
+        return <img key={`${index * 5}shit`} src={image.default} alt="info" />;
+      })}
     </div>
   );
 }
