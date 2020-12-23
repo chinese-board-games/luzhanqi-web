cc662e84081ddb3a0b2d10efda71fc681e0720e3
fireteam99 <raymancheungsy@gmail.com>
2020-12-23 17:41:18 -0500

    added non engineer railroad moves

--- src/util/getSuccessors.js
+++ src/util/getSuccessors.js
@@ -1,36 +1,34 @@
 function getSuccessors(board, x, y, affiliation) {
     // validate the board
     if (board.length != 13) {
-        throw 'Invalid number of rows'
+        throw 'Invalid number of rows';
     }
 
     if (!board.every(row => row.length == 5)) {
-        throw 'Invalid number of columns'
+        throw 'Invalid number of columns';
     }
 
     // validate from
     if (!validX(x)) {
-        throw 'Invalid x'
+        throw 'Invalid x';
     }
 
     if (!validY(y)) {
-        throw 'Invalid y'
+        throw 'Invalid y';
     }
 
     if (isMountainRange(x, y)) {
-        throw 'Invalid "from" coordinates'
+        throw 'Invalid "from" coordinates';
     }
 
-    piece = board[y][x]
+    const piece = board[y][x];
 
     // get the piece type
     if (piece == null) {
-        return []
+        return [];
     }
 
-    const validDestination = (x, y) => validX(x) && validY(y) && board[y][x].affiliation !== affiliation;
-    
-    moves = []
+    const isValidDestination = (x, y) => validX(x) && validY(y) && board[y][x].affiliation !== affiliation;
 
     switch (piece.name) {
         case 'landmine':
@@ -43,16 +41,31 @@ function getSuccessors(board, x, y, affiliation) {
             // } else {
 
             // }
-            return []
+            return [];
         }
         default: {
+            const moves = []
             if (isRailroad(x, y)) {
-                return []
+                const directions = [[-1, 0], [0, -1], [1, 0], [0, 1]];
+                const moves = directions.map(direction => {
+                    const [incX, incY] = direction;
+
+                    let curX = x + incX;
+                    let curY = y + incY 
+                    while (isValidDestination(curX, curY)) {
+                        moves.push([curX, curY])
+                        curX += incX
+                        curY += incY
+                    }
+                });
+                
+
+                return moves
             } else {
                 for (const i = -1; i <= 1; i++) {
                     for (const j = -1; j <= 1; j++) {
-                        if (validDestination(x + i, y + j)) {
-                            moves.push([x + i, y + j])
+                        if (i != 0 && j != 0 && isValidDestination(x + i, y + j)) {
+                            moves.push([x + i, y + j]);
                         }
                     }
                 }
