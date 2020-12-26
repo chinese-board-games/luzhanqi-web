afe4963c08448a1dff4f35f3d8a15d8bddc5c7ca
fireteam99 <raymancheungsy@gmail.com>
2020-12-26 00:30:33 -0500

    added tests for isMountainPass

--- src/util/getSuccessors.test.js
+++ src/util/getSuccessors.test.js
@@ -24,3 +24,43 @@ test("-1 should not be a valid x value", () => expect(validX(-1)).toBe(false));
 test("0 should be a valid x value", () => expect(validX(0)).toBe(true));
 test("4 should be a valid x value", () => expect(validX(4)).toBe(true));
 test("5 should not be a valid x value", () => expect(validX(5)).toBe(false));
+
+const generateRow = y => [...Array(4).keys()].map(x => [x, y]);
+
+const topRailCoords = generateRow(1);
+test("[0, 1] to [4, 1] should be valid railroad coordinates", () =>
+  expect(topRailCoords.map(([x, y]) => isRailroad(x, y)).every((v) => v)).toBe(
+    true
+));
+
+const topMidRailCoords = generateRow(5);
+test("[0, 5] to [4, 5] should be valid railroad coordinates", () =>
+  expect(topMidRailCoords.map(([x, y]) => isRailroad(x, y)).every((v) => v)).toBe(
+    true
+));
+
+const botMidRailCoords = generateRow(7);
+test("[0, 7] to [4, 7] should be valid railroad coordinates", () =>
+  expect(botMidRailCoords.map(([x, y]) => isRailroad(x, y)).every((v) => v)).toBe(
+    true
+));
+
+const botRailCoords = generateRow(11);
+test("[0, 11] to [4, 11] should be valid railroad coordinates", () =>
+  expect(botRailCoords.map(([x, y]) => isRailroad(x, y)).every((v) => v)).toBe(
+    true
+));
+
+const generateCol = x => [...Array(13).keys()].map(y => [x, y]);
+const leftCol = generateCol(0).slice(1, -1);
+test("[0, 1] to [0, 11] should be valid railroad coordinates", () =>
+  expect(leftCol.map(([x, y]) => isRailroad(x, y)).every((v) => v)).toBe(
+    true
+));
+
+const rightCol = generateCol(4).slice(1, -1);;
+test("[4, 1] to [4, 11] should be valid railroad coordinates", () =>
+  expect(rightCol.map(([x, y]) => isRailroad(x, y)).every((v) => v)).toBe(
+    true
+));
+
