9244dd7aa4874efb7ad7e8d16202bbd381d08736
Ian <iankwanhou@gmail.com>
2022-12-18 22:24:30 -0700

    Google OAuth

---
+++ src/components/Login/Google.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+const Google = () => {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const { user } = result;
+        // ...
+        console.log(`credential: ${JSON.stringify(credential)}`);
+        console.log(`token: ${token}`);
+        console.log(`user: ${JSON.stringify(user)}`);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const { email } = error.customData;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+        console.log(`errorCode: ${errorCode}`);
+        console.log(`errorMessage: ${errorMessage}`);
+        console.log(`email: ${email}`);
+        console.log(`credential: ${JSON.stringify(credential)}`);
+      });
+  };
+
+  return (
+    <Button variant="outline-primary" onClick={handleGoogleSignIn}>
+      Sign in with Google
+    </Button>
+  );
+};
+
+export default Google;
