456678a7a952a13be930c097fb2ee73fa2e8866e
Ian Hou <iankwanhou@gmail.com>
2023-02-13 01:59:25 -0500

    reconcile piece keys for new boardsetup, provide gamecontext for affiliation setting

--- src/models/Piece/Piece.js
+++ src/models/Piece/Piece.js
@@ -1,17 +1,17 @@
 /** An object containing basic game information for each piece type. */
 export const pieces = {
-  bomb: { count: 2, order: -1, display: '炸彈' },
-  brigadier_general: { count: 2, order: 6, display: '旅長' },
-  captain: { count: 3, order: 3, display: '連長' },
-  colonel: { count: 2, order: 5, display: '團長' },
-  engineer: { count: 3, order: 1, display: '工兵' },
-  field_marshall: { count: 1, order: 9, display: '司令' },
-  flag: { count: 1, order: 0, display: '軍旗' },
-  general: { count: 1, order: 8, display: '軍長' },
-  landmine: { count: 3, order: -1, display: '地雷' },
-  lieutenant: { count: 3, order: 2, display: '排長' },
-  major_general: { count: 2, order: 7, display: '師長' },
-  major: { count: 2, order: 4, display: '營長' }
+  bomb: { count: 2, order: -1, display: '炸彈', kills: 0 },
+  brigadier_general: { count: 2, order: 6, display: '旅長', kills: 0 },
+  captain: { count: 3, order: 3, display: '連長', kills: 0 },
+  colonel: { count: 2, order: 5, display: '團長', kills: 0 },
+  engineer: { count: 3, order: 1, display: '工兵', kills: 0 },
+  field_marshall: { count: 1, order: 9, display: '司令', kills: 0 },
+  flag: { count: 1, order: 0, display: '軍旗', kills: 0 },
+  general: { count: 1, order: 8, display: '軍長', kills: 0 },
+  landmine: { count: 3, order: -1, display: '地雷', kills: 0 },
+  lieutenant: { count: 3, order: 2, display: '排長', kills: 0 },
+  major_general: { count: 2, order: 7, display: '師長', kills: 0 },
+  major: { count: 2, order: 4, display: '營長', kills: 0 }
 };
 
 export const setupPieces = [];
@@ -30,6 +30,8 @@ Object.keys(pieces).forEach((name) => {
  * @property {number} affiliation
  * @property {number} order
  * @property {number} kills
+ * @property {string} display
+ * @property {string} id
  */
 
 /**
