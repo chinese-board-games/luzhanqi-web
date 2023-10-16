4fa211a37d17c814176973389de0fa522bcbabd9
Ian K Hou <45278651+iankhou@users.noreply.github.com>
2023-10-16 12:49:12 -0400

    Landing page looks a little better

--- src/views/Menu.js
+++ src/views/Menu.js
@@ -1,13 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Container } from '@mantine/core';
 
 function Menu() {
+  const stackStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '0.5em 0.5em'
+  };
+  const cardStyle = {
+    backgroundColor: '#adcdff',
+    padding: '1em',
+    width: '20em',
+    borderRadius: '0.5em',
+    boxShadow: '0.3em 0.3em 0.1em #69a2ff'
+  };
+  const cardContentStyle = {
+    display: 'flex',
+    gap: '0.3em 0.3em'
+  };
+  const linkStyle = { color: 'white' };
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <Link to="/game">Create Match</Link>
-      <Link to="/game">Join/Rejoin Match</Link>
-      <Link to="/setup-test">Test Setup</Link>
-      <Link to="/gameboard-test">Test New Board</Link>
+    <div style={{ display: 'flex', flexDirection: 'column', margin: '1em 1em' }}>
+      <Container style={stackStyle}>
+        <Container style={cardStyle}>
+          <h2>Host a New Game</h2>
+          <Button>
+            <Link to="/game" style={linkStyle}>
+              Create Match
+            </Link>
+          </Button>
+        </Container>
+        <Container style={cardStyle}>
+          <h2>Join an Existing Game</h2>
+          <Button>
+            <Link to="/game" style={linkStyle}>
+              Join Match
+            </Link>
+          </Button>
+        </Container>
+        <Container style={cardStyle}>
+          <h2>For Developers</h2>
+          <Container style={cardContentStyle}>
+            <Button>
+              <Link to="/setup-test" style={linkStyle}>
+                Test Setup
+              </Link>
+            </Button>
+            <Button>
+              <Link to="/gameboard-test" style={linkStyle}>
+                Test New Board
+              </Link>
+            </Button>
+          </Container>
+        </Container>
+      </Container>
     </div>
   );
 }
