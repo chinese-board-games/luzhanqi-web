18aabb00d2305f0fde3ea2fc7967acaeefaa04fd
Ian K Hou <iankwanhou@gmail.com>
2023-10-17 19:18:03 -0400

    no socket before Game component

--- src/components/Lobby/Lobby.js
+++ src/components/Lobby/Lobby.js
@@ -1,122 +1,30 @@
 /* eslint-disable no-console */
 import React, { useContext } from 'react';
-import { useForm } from '@mantine/form';
-import { Button, TextInput, Container } from '@mantine/core';
+import { Button, Container, Title } from '@mantine/core';
 import { GameContext } from 'contexts/GameContext';
 import { useFirebaseAuth } from 'contexts/FirebaseContext';
 
 const Lobby = () => {
   const gameState = useContext(GameContext);
   const { socket } = gameState;
-  const { playerName } = gameState.playerName;
-  const { roomId, setRoomId } = gameState.roomId;
-  const { host, setHost } = gameState.host;
+  const { roomId } = gameState.roomId;
+  const { host } = gameState.host;
   const { joinedGame } = gameState.joinedGame;
-  const { playerList } = gameState.playerList;
-  const { setErrors } = gameState.errors;
-  // const { storedPlayerName, setStoredPlayerName } = gameState.storedPlayerName;
-  // const { storedRoomId, setStoredRoomId } = gameState.storedRoomId;
-  // const { storedPlayerList, setStoredPlayerList } = gameState.storedPlayerList;
   const user = useFirebaseAuth();
-
-  // const [rejoin, setRejoin] = useState(false);
-
-  // useEffect(() => {
-  //   setStoredPlayerName(window.sessionStorage.getItem('playerName'));
-  //   setStoredRoomId(window.sessionStorage.getItem('roomId'));
-  //   setStoredPlayerList(window.sessionStorage.getItem('playerList') || []);
-  // }, []);
-
-  // useEffect(() => {
-  //   if (storedPlayerName && storedRoomId && storedPlayerList.length) {
-  //     setRejoin(true);
-  //   }
-  //   return () => {
-  //     setRejoin(false);
-  //   };
-  // }, [storedPlayerName, storedRoomId, storedPlayerList]);
-
-  /** Tell the server to create a new game */
-  const createNewGame = () => {
-    if (playerName) {
-      socket.emit('hostCreateNewGame', { playerName, hostId: user?.uid || null });
-      setHost(true);
-    } else {
-      setErrors((prevErrors) => [...prevErrors, 'You must provide a username.']);
-    }
-  };
 
   /** Tell server to begin game */
   const roomFull = () => {
     socket.emit('hostRoomFull', roomId);
   };
 
-  /** Attempt to join a game by game ID */
-  const joinGame = ({ playerName, roomId }) => {
-    if (playerName && roomId) {
-      console.log(`Attempting to join game ${roomId} as ${playerName} with clientId ${user?.uid}`);
-      socket.emit('playerJoinGame', {
-        playerName,
-        clientId: user?.uid || null,
-        joinRoomId: roomId,
-        playerList
-      });
-      setRoomId(roomId);
-    } else {
-      setErrors((prevErrors) => [
-        ...prevErrors,
-        'You must provide both a game number and a player name.'
-      ]);
-    }
-  };
-
-  // /** Attempt to rejoin a game by game ID */
-  // const rejoinGame = (e) => {
-  //   e.preventDefault();
-  //   if (storedPlayerName && storedRoomId) {
-  //     console.log(`Attempting to rejoin game ${roomId} as ${playerName}`);
-  //     socket.emit('playerRejoinGame', {
-  //       storedPlayerName,
-  //       storedRoomId,
-  //       storedPlayerList
-  //     });
-  //   } else {
-  //     console.error('playerName and storedRoomId not stored, cannot rejoin game.');
-  //   }
-  // };
-
-  const form = useForm({
-    initialValues: {
-      // these keys must match the input keys for joinGame
-      playerName,
-      roomId
-    }
-  });
-
-  // eslint-disable-next-line no-unused-vars
-  const handleError = (_errors) => {
-    console.log('Form error handled serverside');
-  };
-  const handleSubmit = (values) => {
-    joinGame(values);
-  };
   return (
     <Container style={{ backgroundColor: '#d0edf5' }}>
-      {
-        /** There is no assigned room, give option to create room */
-        roomId ? null : (
-          <Button variant="success" onClick={createNewGame} style={{ width: '12em' }}>
-            Create New Game
-          </Button>
-        )
-      }
-
       {
         /** You have joined the game and are waiting for the host to start */
         joinedGame && !host ? (
           <>
-            <h3>請等主持人</h3>
-            <h3>Waiting for the host</h3>
+            <Title order={3}>請等主持人</Title>
+            <Title order={3}>Waiting for the host</Title>
           </>
         ) : null
       }
@@ -125,8 +33,8 @@ const Lobby = () => {
         /** Give host ability to start game */
         host ? (
           <>
-            <h3>按 &quot;Room Full&quot; 開始遊戲</h3>
-            <h3>Click &quot;Room Full&quot; to begin the game</h3>
+            <Title order={3}>按 &quot;Room Full&quot; 開始遊戲</Title>
+            <Title order={3}>Click &quot;Room Full&quot; to begin the game</Title>
             <Button type="button" variant="success" onClick={roomFull} style={{ width: '8em' }}>
               Room Full
             </Button>
@@ -138,35 +46,13 @@ const Lobby = () => {
         /** Not host and haven't joined a game, give option to join game */
         host || joinedGame ? null : (
           <>
-            {/* disabled until fully implemented */}
-            {/* {rejoin ? (
-              <Button variant="warning" onClick={rejoinGame}>
-                Rejoin
-              </Button>
-            ) : null} */}
             {user ? (
               <>
-                <h2>Hello {user.email}</h2>
+                <Title order={2}>Hello {user.email}</Title>
               </>
             ) : (
-              <div />
+              <Container />
             )}
-            <form onSubmit={form.onSubmit(handleSubmit, handleError)}>
-              <TextInput
-                label="Player name:"
-                placeholder="Ex. Ian"
-                {...form.getInputProps('playerName')}
-              />
-              <TextInput
-                label="Join game:"
-                placeholder="Ex. 12345"
-                {...form.getInputProps('roomId')}
-              />
-              <br />
-              <Button variant="info" type="submit">
-                Submit
-              </Button>
-            </form>
           </>
         )
       }
--- src/views/Menu.js
+++ src/views/Menu.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { GameContext } from 'contexts/GameContext';
 import { Link } from 'react-router-dom';
-import { Button, Container } from '@mantine/core';
+import { ToastContainer, toast } from 'react-toastify';
+import { Button, Container, TextInput } from '@mantine/core';
+import { useFirebaseAuth } from 'contexts/FirebaseContext';
+import { useForm } from '@mantine/form';
+import { Title } from '@mantine/core';
 
 function Menu() {
+  const {
+    socket,
+    playerName: { playerName },
+    roomId: { roomId, setRoomId },
+    playerList: { playerList },
+    host: { setHost },
+    errors: { errors, setErrors }
+  } = useContext(GameContext);
+
+  const createForm = useForm({
+    initialValues: { playerName }
+  });
+
+  const joinForm = useForm({
+    initialValues: {
+      // these keys must match the input keys for joinGame
+      playerName,
+      roomId
+    }
+  });
+
+  const user = useFirebaseAuth();
+
+  /** Clear errors after 1 second each */
+  useEffect(() => {
+    errors.forEach((error) => {
+      toast.error(error, {
+        toastId: `${Date.now()}`
+      });
+    });
+    setErrors([]);
+  }, [JSON.stringify(errors), toast.error]);
+
   const viewStyle = {
-    height: '100vh',
     backgroundColor: '#d0edf5',
     display: 'flex',
     flexDirection: 'column',
@@ -28,34 +65,101 @@ function Menu() {
   };
   const linkStyle = { color: 'white' };
 
+  /** Tell the server to create a new game */
+  const createNewGame = (name) => {
+    if (name) {
+      socket.emit('hostCreateNewGame', { playerName: name, hostId: user?.uid || null });
+      setHost(true);
+      // GameContext will redirect to /game when socket starts the game
+    } else {
+      setErrors((prevErrors) => [...prevErrors, 'You must provide a player name.']);
+    }
+  };
+
+  /** Attempt to join a game by game ID */
+  const joinGame = (playerName, roomId) => {
+    if (playerName && roomId) {
+      console.log(`Attempting to join game ${roomId} as ${playerName} with clientId ${user?.uid}`);
+      socket.emit('playerJoinGame', {
+        playerName,
+        clientId: user?.uid || null,
+        joinRoomId: roomId,
+        playerList
+      });
+      setRoomId(roomId);
+    } else {
+      setErrors((prevErrors) => [
+        ...prevErrors,
+        'You must provide both a game number and a player name.'
+      ]);
+    }
+  };
+
+  const handleCreateSubmit = ({ playerName }) => {
+    createNewGame(playerName);
+    setErrors((prevErrors) => [...prevErrors, 'Unable to connect to server.']);
+  };
+
+  const handleJoinSubmit = ({ playerName, roomId }) => {
+    joinGame(playerName, roomId);
+    setErrors((prevErrors) => [...prevErrors, 'Unable to connect to server.']);
+  };
+
   return (
-    <div style={viewStyle}>
-      <Container style={stackStyle}>
-        <Container style={cardStyle}>
-          <h2>Host a New Game</h2>
-          <Link to="/game" style={linkStyle}>
-            <Button>Create Match</Button>
-          </Link>
-        </Container>
-        <Container style={cardStyle}>
-          <h2>Join an Existing Game</h2>
-          <Link to="/game" style={linkStyle}>
-            <Button>Join Match</Button>
-          </Link>
-        </Container>
-        <Container style={cardStyle}>
-          <h2>For Developers</h2>
-          <Container style={cardContentStyle}>
-            <Link to="/setup-test" style={linkStyle}>
-              <Button>Test Setup</Button>
-            </Link>
-            <Link to="/gameboard-test" style={linkStyle}>
-              <Button>Test New Board</Button>
-            </Link>
+    <>
+      <Container style={viewStyle}>
+        <Container style={stackStyle}>
+          <Container style={cardStyle}>
+            <Title order={3}>Host a New Game</Title>
+            <form onSubmit={createForm.onSubmit(handleCreateSubmit)}>
+              <TextInput
+                label="Player name:"
+                placeholder="Ex. Ian"
+                {...createForm.getInputProps('playerName')}
+              />
+              <br />
+              <Button variant="info" type="submit">
+                Create Match
+              </Button>
+            </form>
+          </Container>
+          <Container style={cardStyle}>
+            <Title order={3}>Join a Game</Title>
+            {/* <Link to="/game" style={linkStyle}>
+              <Button>Join Match</Button>
+            </Link> */}
+            <form onSubmit={joinForm.onSubmit(handleJoinSubmit)}>
+              <TextInput
+                label="Player name:"
+                placeholder="Ex. Ian"
+                {...joinForm.getInputProps('playerName')}
+              />
+              <TextInput
+                label="Join game:"
+                placeholder="Ex. 12345"
+                {...joinForm.getInputProps('roomId')}
+              />
+              <br />
+              <Button variant="info" type="submit">
+                Submit
+              </Button>
+            </form>
+          </Container>
+          <Container style={cardStyle}>
+            <Title order={3}>For developers</Title>
+            <Container style={cardContentStyle}>
+              <Link to="/setup-test" style={linkStyle}>
+                <Button>Test Setup</Button>
+              </Link>
+              <Link to="/gameboard-test" style={linkStyle}>
+                <Button>Test New Board</Button>
+              </Link>
+            </Container>
           </Container>
         </Container>
       </Container>
-    </div>
+      <ToastContainer />
+    </>
   );
 }
 
