e4dbc5197cd3f5d65dde3dc35c9ad4f7aff842a7
Ian K Hou <iankwanhou@gmail.com>
2023-11-03 20:08:31 -0400

    Reset state if user leaves game in Lobby, socket interactions, changeover to use MongoDB _id for rooms

--- src/api/Game.js
+++ src/api/Game.js
@@ -2,7 +2,6 @@
 import axios from 'axios';
 
 export const getGameById = async (gameId) => {
-  console.log(`getGame: ${gameId} `);
   return axios
     .get(`/games/${gameId}`)
     .then((res) => {
